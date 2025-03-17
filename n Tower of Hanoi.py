1ec4fc2fe99435e3de1d0d8fb387164ff11b598f
Ashish1455 <127438366+Ashish1455@users.noreply.github.com>
2025-03-17 18:38:13 +0530

    Update n Tower of Hanoi.py

--- n Tower of Hanoi.py
+++ n Tower of Hanoi.py
@@ -13,27 +13,30 @@ class Stack(Generic[T]):
 def hanoi(begin: int, end: int, n: int, t: int):
     for i in range(2, t-1):
         towers[i].push(towers[begin].pop())
+        print(*towers)
     hanoi3(towers[0], towers[t-1], towers[1], n-t+3)
     for i in range(t-2, 1, -1):
         towers[end].push(towers[i].pop())
+        print(*towers)
 
 def hanoi3(begin: Stack[int], end: Stack[int], temp: Stack[int], n: int) -> None:
     if n == 1:
         end.push(begin.pop())
+        print(*towers)
     else:
         hanoi3(begin, temp, end, n - 1)
         hanoi3(begin, end, temp, 1)
         hanoi3(temp, end, begin, n - 1)
 
 if __name__ == '__main__':
-    num_discs: int = int(input())
+    num_discs: int = int(input('Number of Disks: '))
     num_towers: int = int(input('Number of Towers: '))
     towers = []
     for i in range(num_towers):
         temp: Stack[int] = Stack()
         towers.append(temp)
         del temp
-    for i in range(1, num_discs+1):
+    for i in range(num_discs, 0, -1):
         towers[0].push(i)
     hanoi(0, num_towers-1, num_discs, num_towers)
     print(towers)
