f759bde5f30b90b9ed99d343cc0488fec1a632f9
ashish tandi <ashishtandi07@gmail.com>
2025-03-18 21:24:17 +0530

    Updated files with Visualizer

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    num_discs = int(input('Number of Disks: '))
-    num_towers = int(input('Number of Towers: '))
-    
-    solver = HanoiSolver(num_discs, num_towers)
-    solver.hanoi(0, num_towers - 1, num_discs, num_towers)
-
-    print("Final Tower State:", solver.get_towers())
